1165807f504102385d9d757c4e064ccbd027c0df
Juan Cadavid <juan.cadavid-p@escuelaing.edu.co>
2021-10-19 21:02:01 -0500

    CVDS-2: Update create category

--- src/main/database/tables.sql
+++ src/main/database/tables.sql
@@ -18,12 +18,12 @@ CREATE TABLE IF NOT EXISTS DB_USER(
 -- Table `DB_Category`
 -- -----------------------------------------------------
 CREATE TABLE DB_Category(
-Id SERIAL NOT NULL, 
+Id SERIAL PRIMARY KEY, 
 Name VARCHAR(50) NOT NULL, 
 Description VARCHAR(150) NOT NULL, 
 Date_Create DATE NOT NULL, 
 State VARCHAR(40) NOT NULL, 
-Date_Modification  DATE NOT NULL, 
+Date_Modification  DATE, 
 CONSTRAINT UC_Category UNIQUE (Name)
 );
 
