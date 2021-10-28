45cb09ec8d5bb42f5153cfd7870b0661fec6b5e4
Juan David Murillo <juancho20sp@gmail.com>
2021-10-28 09:28:14 -0500

    CVDS-1: Added populate and xpopulate

---
+++ src/main/database/populate.sql
@@ -0,0 +1,1 @@
+ INSERT INTO DB_USER (userid, status, firstname, lastname, email, username, password, role, maxneeds) VALUES (1026306639, 'ENABLED', 'Juan David', 'Murillo', 'juan.murillo-g@mail.escuelaing.edu.co', 'juancho20sp', 'admin', 'ADMIN', 5);
