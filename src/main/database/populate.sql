b83c2663755ef466741a585d09bc2eb38fdab22f
Laura Garcia <laura.garcia-l@outlook.com>
2021-11-09 08:34:14 -0500

    Update DB_OFFERS

--- src/main/database/populate.sql
+++ src/main/database/populate.sql
@@ -2,3 +2,9 @@
 -- Table `DB_USER`
 -- -----------------------------------------------------
  INSERT INTO DB_USER (userid, status, firstname, lastname, email, username, password, role, maxneeds) VALUES (1026306639, 'ENABLED', 'Juan David', 'Murillo', 'juan.murillo-g@mail.escuelaing.edu.co', 'juancho20sp', 'admin', 'ADMIN', 5);
+
+
+ -- -----------------------------------------------------
+ -- Table `DB_OFFERs`
+ -- -----------------------------------------------------
+ INSERT INTO DB_OFFERS (offerCategory, name, description, status, userid, maxneeds) VALUES (7, 'Prueba2', 'Esto es una prueba', 'ACTIVE', 1001184238, 7)
